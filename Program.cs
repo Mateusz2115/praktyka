1d61b45b2abefc33fed3db581df0a9d902c09d71
Matiz <matiz76543@gmail.com>
2021-04-20 23:03:08 +0200

    fff

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[5];
+            for (int i = 0; i < 5; i++)
+                tab[i] = i;
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(i);
+
+        }
+    }
+}
