7b58a44db08a53eb982e71374670cf01b3e06bf0
Matiz <matiz76543@gmail.com>
2021-04-21 02:48:24 +0200

    zadanka

---
+++ zad1.cs
@@ -0,0 +1,54 @@
+﻿
+sortedArray; //posortowana tablic
+int arraySize = sortedArray.Length;  //rozmiar tablicy
+element; //szukany element
+
+for(int i = 0; i < arraySize; i++)
+{
+    if(sortedArray[i] == element)
+    {
+        if(i+1 < arraySize && sortedArray[i] == sortedArray[i+1])
+        {
+            sortedArray[i] = null;
+            continue;
+        }
+        else
+        {
+            sortedArray[i] = null;
+            break; 
+        }
+    }
+}
+
+///////////////////////////////////////////DRUGI PUNKT
+int sizeA = A.size();
+int sizeB = B.size();
+//zmienna j służy do nawigowania po liście, gdy usuniemy element to zmienią się pozycje elementów w liście więc j jest potrzebna żeby
+//nie wyjśc poza zakres oraz odpowiednio sprawdzić wszytkie elementy
+for(int i = 0, j = 0; i < sizeA; i++)
+{
+    Element element = A.get(j);
+    int key = element.getKey();
+    bool isFound = false;  //informuje czy coś znależliśmy 
+    for (int k = 0; k < sizeB; k++) //sprawdzamy czy znajduje się element o tym samym kluczu
+    {
+        Element element2 = B.get(k);
+        int key2 = element2.getKey();
+        if(key == key2)
+        {
+            isFound = true;
+            break;
+        }
+    }
+    if(!isFound)
+    {
+        A.remove(j);
+    }
+    else
+    {
+        j++;
+        isFound = false;
+    }
+
+}
+
---
+++ zad2.cs
@@ -0,0 +1,61 @@
+﻿
+class Dokument
+{
+    protected String issuer;  //wystawca
+    protected String receiver;
+    protected DateTime date;
+    protected int number;
+
+    public Dokument(String issuer, String receiver, DateTime date, int number)
+    {
+        this.issuer = issuer;
+        this.receiver = receiver;
+        this.date = date;
+        this.number = number;
+    }
+
+    public void getRabat()
+    {
+        Console.WriteLine("no discount");
+    }
+    
+    String getIssuer() { return issuer; }
+
+    String geReceiver() { return receiver; }
+
+    DateTime getDate() { return date; }
+
+    int getNumber() { return number; }
+
+}
+
+class Zamowienie : Dokument
+{
+    DateTime realizationDate;
+
+    Zamowienie(String issuer, String receiver, DateTime date, int number, DateTime realizationDate) : base(issuer, receiver, date, number)
+    {
+        this.realizationDate = realizationDate;
+    }
+
+    public String getRabat()
+    {
+        return "Rabat from " + receiver;
+    }
+
+}
+
+class Faktura : Dokument
+{
+    String fakturaType;
+
+    Faktura(String issuer, String receiver, DateTime date, int number, String fakturaType) : base(issuer, receiver, date, number)
+    {
+        this.fakturaType = fakturaType;
+    }
+
+    public String getRabat()
+    {
+        return "Rabat from " + fakturaType + " number: " + number.ToString();
+    }
+}
